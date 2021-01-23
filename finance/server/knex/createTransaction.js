a4b8f52d867dc6fc7b1a4f80d3fba0335857d27c
washerd <washerd2008@gmail.com>
2021-01-23 20:33:17 +0200

    Initial commit

--- finance/server/knex/createTransaction.js
+++
@@ -1,5 +0,0 @@
-import {getKnex} from '.';
-
-const knex = getKnex();
-
-export default async () => new Promise(resolve => knex.transaction(resolve));
--- finance/server/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import {mergeSchemas} from 'apollo-server-micro';
-import schemas from "../schemas";
-import userResolvers from './user';
-
-const resolvers = {...userResolvers}
-
-export default mergeSchemas({schemas, resolvers});
